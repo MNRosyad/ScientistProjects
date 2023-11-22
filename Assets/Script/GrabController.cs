705dfe4c70bdc07a4b52ae49a5f21a5abf928c85
ini Rosyad <101623522+MNRosyad@users.noreply.github.com>
2023-11-23 00:40:01 +0700

    Fixing bug, add door button mechanism
    on Scene Mekanisme

--- Assets/Script/GrabController.cs
+++ Assets/Script/GrabController.cs
@@ -6,55 +6,90 @@ using UnityEngine.InputSystem;
 public class GrabController : MonoBehaviour
 {
     [SerializeField]
-    private Transform grabDetect;
+    private Transform boxUp;
     [SerializeField]
-    private Transform boxHolder;
+    private Transform boxFront;
     [SerializeField]
-    private float rayDistance;
+    private Transform boxDetect;
 
-    private bool grabToggle;
+    [SerializeField]
+    private float rayFrontDistance = 1f;
+    [SerializeField]
+    private float rayUpDistance = 2.2f;
+    private int layerNumber = 6;
+    private int layerMask;
+
+    [SerializeField]
+    private bool grabToggle = false;
+    [SerializeField]
+    private bool defaultRay = true;
     private Collider2D savedCollider;
     private CapsuleCollider2D capsuleCollider;
+    private PlayerController player;
+
+    private RaycastHit2D boxCheck;
 
     private void Awake()
     {
         capsuleCollider = GetComponent<CapsuleCollider2D>();
+        player = GetComponent<PlayerController>();
+
+        layerMask = 1 << layerNumber;
     }
 
-    void Update()
+    private void Update()
     {
-        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.left * transform.localScale, rayDistance);
+        if (defaultRay)
+        {
+            boxCheck = Physics2D.Raycast(boxDetect.position, Vector2.left * transform.localScale, rayFrontDistance, layerMask);
+        }
+        else if (!defaultRay)
+        {
+            boxCheck = Physics2D.Raycast(boxDetect.position, Vector2.up , rayUpDistance, layerMask);
+        }
+
+        Grabbing();
+    }
+
+    private void Grabbing()
+    {
+        Collider2D detectBox = boxCheck.collider;
 
         if (Keyboard.current.eKey.wasPressedThisFrame)
         {
-            grabToggle = !grabToggle;
-            Collider2D detectBox = grabCheck.collider;
-            if (grabToggle == true)
+            if (grabToggle == false)
             {
-                if (detectBox != null && detectBox.tag == "Box")
+                if (detectBox != null && detectBox.CompareTag("Box"))
                 {
-                    detectBox.transform.parent = boxHolder;
-                    detectBox.transform.position = boxHolder.position;
+                    detectBox.transform.parent = boxUp;
+                    detectBox.transform.position = boxUp.position;
                     detectBox.GetComponent<Rigidbody2D>().isKinematic = true;
-                    detectBox.enabled = false;
+                    detectBox.isTrigger = true;
 
-                    capsuleCollider.direction = CapsuleDirection2D.Horizontal;
-                    capsuleCollider.size = new Vector2(2.3f, 2f);
-                    capsuleCollider.offset = new Vector2(-0.7f, -0.4f);
+                    player.AdjustColliderOffset(-0.02f, 0.1f);
+                    player.AdjustColliderSize(0.8f, 3f);
 
-                    grabCheck.rigidbody.velocity = Vector2.zero;
+                    boxCheck.rigidbody.velocity = Vector2.zero;
                     savedCollider = detectBox;
+                    grabToggle = true;
+                    defaultRay = false;
                 }
             }
-            else if (grabToggle == false)
+            else if (grabToggle == true)
             {
-                savedCollider.transform.parent = null;
-                savedCollider.GetComponent<Rigidbody2D>().isKinematic = false;
-                savedCollider.enabled = true;
+                if (detectBox != null && detectBox.CompareTag("Box"))
+                {
+                    savedCollider.transform.parent = null;
+                    savedCollider.transform.position = boxFront.position;
+                    savedCollider.GetComponent<Rigidbody2D>().isKinematic = false;
+                    savedCollider.isTrigger = false;
 
-                capsuleCollider.direction = CapsuleDirection2D.Vertical;
-                capsuleCollider.size = new Vector2(0.8f, 2f);
-                capsuleCollider.offset = new Vector2(-0.02f, -0.4f);
+                    player.AdjustColliderOffset(-0.02f, -0.4f);
+                    player.AdjustColliderSize(0.8f, 2f);
+
+                    grabToggle = false;
+                    defaultRay = true;
+                }
             }
         }
     }
