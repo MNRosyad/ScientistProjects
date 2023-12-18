df41deb5180afda6e5b44ef19f56f694ec018f62
Vikrie90 <146800988+Vikrie90@users.noreply.github.com>
2023-12-19 02:32:09 +0700

    Update
    banyak lah pokoknya

---
+++ Assets/Script/CutsceneAkhir.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class CutsceneAkhir : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Button startButton;
+
+    void Start()
+    {
+        // Mendengarkan kejadian video selesai diputar
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    // Fungsi yang akan dipanggil saat video selesai diputar
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        // Aktifkan tombol UI
+        startButton.gameObject.SetActive(true);
+    }
+
+    public void LoadGameplayScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
